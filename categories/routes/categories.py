c746f7f9546fe845be32e10829234a1029362ffe
zLucky090802 <d.espitia15@gmail.com>
2025-05-30 21:58:39 -0500

    create categorie method make it

--- categories/routes/categories.py
+++ categories/routes/categories.py
@@ -2,12 +2,13 @@ from fastapi import APIRouter, Path
 from schemas.categories import BaseModel, CategoriaBase, CategoriaCreate, CategoriaOut, CategoriaUpdate, Optional
 from services.categories_services import (
     get_categorias,
-    get_categorias_by_id
+    get_categorias_by_id,
+    create_categorie
 )
 
 router = APIRouter()
 
-@router.get("/usuario/{usuario_id}", response_model= CategoriaOut)
+@router.get("/usuario/{usuario_id}", response_model= list[CategoriaOut])
 async def get_categorias_router(usuario_id: int = Path(...)):
     return await get_categorias(usuario_id)
 # async def get_categorias_router(user_id: int):
@@ -16,3 +17,7 @@ async def get_categorias_router(usuario_id: int = Path(...)):
 @router.get('/{id}', response_model=CategoriaOut)
 async def get_categorias_by_id_router(id: int = Path(...)):
     return await get_categorias_by_id(id)
+
+@router.post('/', response_model=CategoriaBase)
+async def create_categorie_router(categorie:CategoriaBase):
+    return await create_categorie(categorie)
--- categories/services/categories_services.py
+++ categories/services/categories_services.py
@@ -1,7 +1,8 @@
 from schemas.categories import BaseModel, CategoriaBase, CategoriaCreate, CategoriaOut, CategoriaUpdate,Optional
 from fastapi import HTTPException, status
 from prisma_client import db
-
+from fastapi.responses import JSONResponse
+from prisma.errors import PrismaError
 
 async def get_categorias(user_id:int):
     user_id = user_id
@@ -28,4 +29,45 @@ async def get_categorias_by_id(id: int):
         return categoria
     else:
         raise HTTPException(status_code=400, detail='Categoria no encontrada')
+
+
+async def create_categorie(categorie: CategoriaBase) -> CategoriaBase:
+
+    if not categorie.nombre:
+        raise HTTPException(status_code=400, detail='El nombre es requerido')
+
+    if not categorie.usuario_id:
+        raise HTTPException(status_code=400, detail='El id del usuario es requerido')
+
+    try:
+        existing = await db.categorias.find_first(
+            where={
+                "usuario_id": categorie.usuario_id,
+                "nombre": categorie.nombre
+            }
+        )
+        if existing:
+            print('Entrando al if de categoría existente')
+            raise HTTPException(
+                status_code=400,
+                detail='La categoría ya existe para este usuario'
+            )
+        
+        nueva_categoria = await db.categorias.create(
+            data=categorie.model_dump()
+        )
+
+        return nueva_categoria
+
+    except PrismaError as e:
+        print("⛔ Error al crear categoría:", e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail='Error al acceder a la base de datos'
+        )
+
+   
+
+
+
     
